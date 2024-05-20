1b19af01e87054d318bda2c0fd48721c14095bce
Karan Patel <k.patel1121993@gmail.com>
2024-05-21 00:36:10 +0530

    Video Title and Movie Trailer

---
+++ src/utils/constants.js
@@ -0,0 +1,14 @@
+export const LOGO =
+  "https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png";
+
+export const USER_AVATAR =
+  "https://occ-0-2890-2186.1.nflxso.net/dnm/api/v6/vN7bi_My87NPKvsBoib006Llxzg/AAAABbrdoxu2y6ytbozzr79fLlJ06sLyYtIPNX2DPmWQWUWX0fPS3kXI8KnCpj2J4P0L6Bu2kJlfK_4IVcLuZX_1FAOBkN8KoNFUTGVE.png?r=64c";
+
+export const API_OPTIONS = {
+  method: "GET",
+  headers: {
+    accept: "application/json",
+    Authorization:
+      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkYmUzNmE5MGY2YTAzNzVmNTI3MGIwM2M3NzBhZDRmZSIsInN1YiI6IjY2NDk5OGM2NDRlYjRmNmQwYTkyZTc3YiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.LdSbIDA-JrtLnt20At1aOrajzlFoqxCngS6nJL-68pg",
+  },
+};
